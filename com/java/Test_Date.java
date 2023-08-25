b1f48f55a5f2a2b6fead064361322542c7e4b006
zhang-yi5058 <505831532@qq.com>
2023-08-25 16:08:10 +0800

    notes

---
+++ com/java/Test_Date.java
@@ -0,0 +1,20 @@
+package com.java;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author SiYi
+ * @version 1.0
+ * @date 2023/8/20 11:09
+ * @desciption:
+ */
+public class Test_Date {
+    public static void main(String[] args) {
+        LocalDateTime ldt = LocalDateTime.now();
+        int a = ldt.getYear();
+        System.out.println(a);
+
+    }
+
+}
--- com/java/homework/day10String2/Longest_qinazhui.java
+++ com/java/homework/day10String2/Longest_qinazhui.java
@@ -9,38 +9,38 @@ package com.java.homework.day10String2;
 public class Longest_qinazhui {
     public static void main(String[] args) {
 
-        String[] arr = {"flower", "flow", "flight"};
-        String[] arr2 = {"dog", "racecar", "car"};
-        StringBuilder sb = new StringBuilder(arr[0]);
-        sb.deleteCharAt(arr[0].length() - 1 - 0);
-        sb.deleteCharAt(arr[0].length() - 1 - 1);
-//        sb.deleteCharAt(arr[0].length() - 1 - 2);
+        String[] strs = {"flower", "flow", "flight"};
+        String[] strs2 = {"dog", "racecar", "car"};
+        StringBuilder sb = new StringBuilder(strs[0]);
+        sb.deleteCharAt(strs[0].length() - 1 - 0);
+        sb.deleteCharAt(strs[0].length() - 1 - 1);
+//        sb.deleteCharAt(strs[0].length() - 1 - 2);
 //        System.out.println(sb);
-//        System.out.println(arr[1]);
-//        System.out.println(sb.toString().equals(arr2[1]));
+//        System.out.println(strs[1]);
+//        System.out.println(sb.toString().equals(strs2[1]));
         // System.out.println(sb.length());
 
 
-        String result = Long(arr2);
+        String result = Long(strs2);
         System.out.println(result);
     }
 
-    public static String Long(String[] arr) {
+    public static String Long(String[] strs) {
 
         int index = 1;
         int i = 0;
-        StringBuilder sb = new StringBuilder(arr[0]);
-        while (index < arr.length) {
-            int length = Math.min(sb.length(), arr[index].length());  // 获取较小的长度
+        StringBuilder sb = new StringBuilder(strs[0]);
+        while (index < strs.length) {
+            int length = Math.min(sb.length(), strs[index].length());  // 获取较小的长度
 
-            if (sb.toString().equals(arr[index].substring(0, length))) {
+            if (sb.toString().equals(strs[index].substring(0, length))) {
                 //true
                 index++;
                 continue;
             } else {
                 //false
-                if (i != arr[0].length() - 1) {
-                    sb.deleteCharAt(arr[0].length() - 1 - i);
+                if (i != strs[0].length() - 1) {
+                    sb.deleteCharAt(strs[0].length() - 1 - i);
                     i++;
                     continue;
                 } else {
