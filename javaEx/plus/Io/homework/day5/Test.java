929d70444f238e3e68bb601e6fd1ffda9d157abc
zhang-yi5058 <505831532@qq.com>
2023-09-16 20:39:44 +0800

    (0916)20.39

---
+++ javaEx/plus/Io/homework/day5/Test.java
@@ -0,0 +1,16 @@
+package javaEx.plus.Io.homework.day5;
+
+import java.io.FileReader;
+
+/**
+ * @author SiYi
+ * @version 1.0
+ * @date 2023/9/16 11:40
+ * @desciption:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println('a'^'b');
+
+    }
+}
